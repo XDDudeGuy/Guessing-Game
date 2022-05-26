49fe97b8a0aa3c418cb4a39811424007a567fcb7
Caleb Allen <XDDudeGuy416@protonmail.ch>
2022-05-25 20:06:30 -0400

    Added Comments and a README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guessing Game
+
+## A very basic guessing game GUI using PySimpleGui
+
+### This repo was made to teach people to use PySimpleGui and get beginners into creating GUIs with python
+
+***Any code can be used anywhere, this is very basic code, I do not need credit***
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import Tuple
+from typing import Tuple # Allows the usage of the Tuple type in our checker function
 from rng import rand_num as rng
 import PySimpleGUI as sg
 
-sg.theme('Dark Amber')
+sg.theme('Dark Amber') # Sets the theme for the gui
 
 def checker(answer: int, guess: int) -> Tuple[bool, str]:
     if answer == guess:
@@ -15,21 +15,21 @@ def checker(answer: int, guess: int) -> Tuple[bool, str]:
 
 layout = [[sg.Text("Guess a number from 1 to 1000:")],
 [sg.InputText(key="--INPUT--"), sg.Button("Submit"), sg.Button("Close")],
-[sg.Text("", key="--OUTPUT--")]]
+[sg.Text("", key="--OUTPUT--")]] # Defines how our window should be layed out
 
-window = sg.Window(title="   Guessing Game", layout=layout)
+window = sg.Window(title="  Guessing Game", layout=layout) # Creates the window 
 
 answer = rng(1, 1000)
 
 while True:
     event, values = window.read()
-    if event == "Close" or event == sg.WIN_CLOSED or event == sg.WIN_CLOSE_ATTEMPTED_EVENT:
+    if event == "Close" or event == sg.WIN_CLOSED or event == sg.WIN_CLOSE_ATTEMPTED_EVENT: # If the user tries to close the window
         break
-    if event == "Submit":
+    if event == "Submit": # if the user submits their answer
         is_answer, output = checker(answer, int(values["--INPUT--"]))
         if is_answer:
             window["--OUTPUT--"].update("YOU WIN!")
         else:
             window["--OUTPUT--"].update(output)
 
-window.close()
+window.close() # closes the window
