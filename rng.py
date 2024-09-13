cefb3e5b4376af02bc3277d99d920909bb1d4d3f
26CAllen <caleb.allen@stu.rowan.kyschools.us>
2024-09-13 16:57:58 +0000

    Simplified stupid stuff I did

--- rng.py
+++
@@ -1,7 +0,0 @@
-from random import randint as r
-from random import seed as s
-
-def rand_num(minimum: int, maximum: int) -> int:
-    for _ in range(0,100):
-        s(r(minimum, maximum))
-    return r(minimum, maximum)
